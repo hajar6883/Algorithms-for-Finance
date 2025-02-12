611c31168b5c26a5746931ecf2bf44329b85e4c6
hajar <hajarlamtaai25@gmail.com>
2025-02-12 01:04:10 +0100

    redo on sarimax

--- StockTSModelling/notebooks/models.py
+++ StockTSModelling/notebooks/models.py
@@ -1,1 +1,13 @@
-# custom_SARIMAX : coverging constrainde problem..
+# custom_SARIMAX : coverging constrained problem..
+
+
+'''
+Unbounded Optimization Problem:
+* (L-BFGS-B) optimiser is struggling because the parameter space is too wide or the initial conditions don’t make sense.
+* Non-Stationary Data: used diff
+* Seasonal Order : validating using fourrier
+* Too Many Parameters:High-order ARIMA models (large p, q, P, Q) are computationally expensive and prone to overfitting.
+
+'''
+
+
